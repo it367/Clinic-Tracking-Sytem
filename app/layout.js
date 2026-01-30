fba6f0aab875ae86b62f7adf089f5b8ebbf98726
it367 <it@kidshinehawaii.com>
2026-01-31 02:30:04 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
 export const metadata = {
   title: 'CMS - KidShine Hawaii',
-  description: 'Clinic Management Portal',
+  description: 'Clinic Management Portal for KidsShine Hawaii',
+  authors: [{ name: 'Mark Murillo' }],
+  creator: 'Mark Murillo',
 }
 export default function RootLayout({ children }) {
   return (
